25b32053b69065f9425f3ad295923b95482433b4
Katie Jenkins <katiejenkinswebdev@gmail.com>
2018-05-26 12:49:40 -0600

    printing messages to screens for 2 users

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ io.on('connection', function(socket){
   console.log('a user connected');
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');

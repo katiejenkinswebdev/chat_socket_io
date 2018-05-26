9242642c6e43329125789b4a39f4339d50f06f3c
Katie Jenkins <katiejenkinswebdev@gmail.com>
2018-05-26 12:47:34 -0600

    loggin messages from 2 users

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+
   socket.on('disconnect', function(){
-  console.log('user disconnected');
-});
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
